b230ee020f9aabb12d0a96d8378f45e58ae82778
robinNcode <msmrobin518@gmail.com>
2026-07-11 14:36:17 +0600

    update: Passport setup complete.

---
+++ backend/routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\v1\AuthController;
+
+// Guest urls
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Welcome to the API',
+    ]);
+})->name('home');
+
+Route::get('login', function () {
+    return response()->json([
+        'code' => 401,
+        'status' => 'error',
+        'message' => 'Please login to continue',
+    ]);
+})->name('login');
+
+Route::get('register', function () {
+    return response()->json([
+        'code' => 401,
+        'status' => 'error',
+        'message' => 'Please register to continue',
+    ]);
+})->name('register');
+
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
