7c4c557af4db7bc3cd3fc40dd2447438b355b7ec
robinNcode <msmrobin518@gmail.com>
2026-07-12 19:36:48 +0600

    feat: implement database seeders for core application entities

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -15,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UsersTableSeeder::class,
+            PostsTableSeeder::class,
+            CommentsTableSeeder::class,
+            LikesTableSeeder::class,
+            OauthClientsTableSeeder::class,
+            OauthAccessTokensTableSeeder::class,
+            
         ]);
     }
 }
